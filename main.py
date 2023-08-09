0ddf9e0c755585094ed5fc0531e9af903690c267
Nathan Owens <nmowens95@gmail.com>
2023-08-08 22:50:29 -0500

    Need to change obstacle 3 to work with odds and evens

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import random
 
 DECISION = "Y/N or q to quit"
 STARTING_BALANCE = 100
-BUYOUT = 500
+BUYOUT = 400
 new_line = "\n"
 decisions = ["y", "n", "q"]
+
 
 print("Hello and welcome to Life's a Gamble! 🎲🎲🎲\n")
 name = input("What's your name? ")
 print('') 
 print(f"Okay {name} the rules will be quite simple, you will wager money on your decisions and if you can manage to make enough money you can buy yourself out.\n\nI shall be your host and guide you, now....")
 
-        
-def play(input):
+
+def play():
     while True:
         play_input = input(f"{new_line}Do you wish to play? {DECISION} ")   
         if play_input.lower() not in decisions:
@@ -24,10 +25,9 @@ def play(input):
             if play_input.lower() == "n" or play_input.lower() == "q":
                 print("Maybe next time then.")
                 exit()
-    
-play(input)
+play()
 
-def start(input):
+def start():
     while True:
         play_input = input(f"{new_line}You have woken up in an unfamiliar place, it seems to be an abandoned casino...There's an envelope with ${STARTING_BALANCE}, pick it up? {DECISION} ")
         if play_input.lower() == "y":
@@ -42,40 +42,83 @@ def start(input):
             if play_input.lower() not in decisions:
                 print(f"{new_line}Please type {DECISION}")
                 continue
-        return balance
-    
+    return balance
 
-start(input)
 
-def obstacle1(input):
-    print(f"{new_line}As you look ahead of you, what you will see is a door. This is the start of the fight for your life.")
-    print("\n")
+def obstacle1():
+    balance = int(start())
+    print(f"{new_line}As you look ahead of you, what you will see is a door. This is your beginning.")
+    random_num = random.randint(1, 2)
+    print(f"{new_line}We'll start easy. Guess a number between 1 and 2, if you're right the first door will open. If not you will have to continue to wager. ")
     while True:
-        random_num = random.randint(1, 2)
-        guess = input("We'll start easy. Guess a number between 1 and 2, if you're right the first door will open. If not you will have to continue to wager. ")
+        if balance <= 0:
+            print(f"{new_line}Life is money and yours is up.\n\n...You are dead😵")
+            quit()
+        guess = input(f"{new_line}Pick 1 or 2: ")
         if guess.isdigit():
             guess = int(guess)
             if guess == random_num:
                 balance += 10
-                print("Looks like you've made it through phase one... Here is a little reward for you. You won $10 and your balance is now {balance}")
+                print(f"{new_line}Looks like you've made it through phase one... Here is a little reward for you. You won $10 and your balance is now ${balance}")
                 break
             elif guess != random_num:
                 balance -= 5
-                print(f"Guess you'll have to try again....your balance is now ${balance}")
-            elif balance == 0:
-                print("Life is money and yours is up.\n\n...You are dead😵")
-                quit()
+                print(f"{new_line}Guess you'll have to try again....your balance is now ${balance}")
+                continue
             else:
-                print("Enter either 1 or 2")
+                print(f"{new_line}Enter either 1 or 2: ")
                 continue
     return balance
 
-obstacle1(input)
 
-def main():
-    balance = start()
+def obstacle2():
+    balance = int(obstacle1())
+    print(f"{new_line}Good, now in this room there is a slot machine with only one column. Take a guess on the choices. The buy in is now $20")
+    print(f"{new_line}You may choose between: King/Queen/Jack ")
+    slot_options = ["king", "queen", "jack"]
+    random_guess = random.randint(0, 2)
+    slot = slot_options[random_guess]
     while True:
-        if obstacle1 > 0:
-            balance = obstacle1()
+        if balance <= 0:
+            print(f"{new_line}Life is money and yours is up.\n\n...You are dead😵")
+            quit()
+        guess = input(f"{new_line}Pick: King/Queen/Jack ")
+        if guess.lower() == slot:
+            balance += 40
+            print(f"{new_line}That's your 2nd obstacle... Here is a little reward for you. You won $40 and your balance is now ${balance}")
+            break
+        elif guess.lower != slot:
+            balance -= 20
+            print(f"{new_line}Guess you'll have to try again....your balance is now ${balance}")
+            continue
+        else:
+            print(f"{new_line}Enter King/Queen/Jack: ")
+            continue
+    return balance
 
-main()
+
+def obstacle3():
+    balance = int(obstacle2())
+    print(f"{new_line}Ahh luck may be with you. Lets up the ante to $50.")
+    print(f"{new_line}On the table to your right is a dice cube. Pick odds or evens: ")
+    dice_roll = random.randint(1, 6)
+    even = dice_roll % 2 == 0
+    odd = dice_roll % 1 == 1
+    while True:
+        if balance <= 0:
+            print(f"{new_line}Life is money and yours is up.\n\n...You are dead😵")
+            quit()
+        guess = input(f"{new_line}Pick between 1 and 6: ")
+        if guess == even or guess == odd:
+            balance += 80
+            print(f"{new_line}That's your 2nd obstacle... Here is a little reward for you. You won $40 and your balance is now ${balance}")
+            break
+        elif guess != even or guess != odd:
+            balance -= 50
+            print(f"{new_line}Guess you'll have to try again....your balance is now ${balance}")
+            continue
+        else:
+            print(f"{new_line}Enter betweeen 1 and 6: ")
+            continue
+    return balance
+obstacle3()
